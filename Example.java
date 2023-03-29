289d9f4ddf7e82bdea23270751980ae05a022dbb
LaxminarayananLoganathan <124677287+LaxminarayananLoganathan@users.noreply.github.com>
2023-03-29 07:58:17 +0530

    First Uploaded

---
+++ Example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Example
+{
+publc static void main(String[]args)
+{
+System.out.println("Apple");
+System.out.println("orange");
+System.out.println("bannana");
+System.out.println("Grapes");
+}
+}
