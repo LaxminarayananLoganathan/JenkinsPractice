c3b8c294e3f4f38fdbe5a7921725f2d5ab80131f
LaxminarayananLoganathan <124677287+LaxminarayananLoganathan@users.noreply.github.com>
2023-03-29 08:56:54 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 class Example
 {
-publc static void main(String[]args)
+public static void main(String[]args)
 {
 System.out.println("Apple");
 System.out.println("orange");
 System.out.println("bannana");
 System.out.println("Grapes");
-  System.out.println("Cevi");
+System.out.println("Cevi");
 }
 }
